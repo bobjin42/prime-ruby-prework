df67d5797d3710f407792b3436a56dd839c402ef
Yicheng Jin <bobjin42@gmail.com>
2018-06-08 17:08:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,13 @@
-def prime? (num)
+def prime(num)
   if(num < 2)
     return false;
+  end
   i = 2;
   while  i < num
   if num % i == 0;
     return false
+  end
   i += 1;
-end
+  end
   return true;
 end
